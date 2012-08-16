ae882ae6684cdb2e7a065a356a171ad8696a20bb
Peter Steinberger <me@petersteinberger.com>
2012-08-16 11:45:01 +0200

    cleanup

--- Example/StackedViewKitExample/AppDelegate.m
+++ Example/StackedViewKitExample/AppDelegate.m
@@ -26,26 +26,14 @@
     // set root controller as stack controller
     ExampleMenuRootController *menuController = [[ExampleMenuRootController alloc] init];
     self.stackController = [[PSStackedViewController alloc] initWithRootViewController:menuController];
+
+    // test to disable large inset
+    //self.stackController.largeLeftInset = self.stackController.leftInset;
+
     self.window.rootViewController = self.stackController;
     [self.window makeKeyAndVisible];
 
     return YES;
-}
-
-
-- (void)applicationWillResignActive:(UIApplication *)application; {
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
 }
 
 @end
